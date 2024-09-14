6fa1fe4d4d8f82cbd077d656bbec6e29e62723d7
David Menache <dmenache@devopsandplatforms.com>
2024-09-14 23:40:39 +0000

    build

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const jsonToCSV = (jsonArray) => {
     csvRows.push(values.join(','));
   });
 
+
   return csvRows.join('\n');
 };
 
@@ -116,26 +117,51 @@ function App() {
     setCurrentPage(1); // Reset to first page when changing search query
   };
 
-  // Download the current view (all filtered records) as CSV
-  const handleDownloadCSV = () => {
-    const query = `http://localhost:5000/contacts/download?city=${selectedCity}&zipCode=${selectedZipCode}&areaCode=${selectedAreaCode}&category=${selectedCategory}&search=${searchQuery}`;
-    axios
-      .get(query)
-      .then(response => {
-        const csv = jsonToCSV(response.data);
-        const blob = new Blob([csv], { type: 'text/csv' });
-        const url = window.URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', 'contacts.csv');
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      })
-      .catch(error => {
-        console.error('Error downloading CSV:', error);
-      });
-  };
+
+// Download the current view (all filtered records) as CSV
+const handleDownloadCSV = () => {
+  const query = `http://localhost:5000/contacts/download?city=${selectedCity}&zipCode=${selectedZipCode}&areaCode=${selectedAreaCode}&category=${selectedCategory}&search=${searchQuery}`;
+  axios({
+    url: query,
+    method: 'GET',
+    responseType: 'blob', // Important: This tells axios to treat the response as a blob (binary data)
+  })
+  .then((response) => {
+    const url = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'contacts.csv'); // Name of the file to be downloaded
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  })
+  .catch(error => {
+    console.error('Error downloading CSV:', error);
+  });
+};
+
+
+
+  //// Download the current view (all filtered records) as CSV
+  //const handleDownloadCSV = () => {
+    //const query = `http://localhost:5000/contacts/download?city=${selectedCity}&zipCode=${selectedZipCode}&areaCode=${selectedAreaCode}&category=${selectedCategory}&search=${searchQuery}`;
+    //axios
+      //.get(query)
+      //.then(response => {
+        //const csv = jsonToCSV(response.data);
+        //const blob = new Blob([csv], { type: 'text/csv' });
+        //const url = window.URL.createObjectURL(blob);
+        //const link = document.createElement('a');
+        //link.href = url;
+        //link.setAttribute('download', 'contacts.csv');
+        //document.body.appendChild(link);
+        //link.click();
+        //document.body.removeChild(link);
+      //})
+      //.catch(error => {
+        //console.error('Error downloading CSV:', error);
+      //});
+  //};
 
   return (
     <div className="container">
